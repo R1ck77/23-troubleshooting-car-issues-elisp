f8674401ae572f42a9831ba0ef3155f382062bad
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-09 18:32:49 +0200

    WIP to pass the first tests

--- car-troubleshooting-mode-test.el
+++ car-troubleshooting-mode-test.el
@@ -1,23 +1,34 @@
+(require 'cl)
 (require 'buttercup)
 (setq load-path (cons "." load-path))
 (require 'car-troubleshooting-mode)
+
+(defun get-argument (args)
+  (first (first args)))
 
 (describe "car-troubleshooting-mode.el"
   (before-each
     (kill-buffer (get-buffer "* Car Troubleshooting Mode *")))
   (describe "car-troubleshooting"
-    (it "creates a buffer with the specified name and mode"
-      (car-troubleshooting-mode)
+    (it "creates a buffer with t%he specified name and mode"
+      (car-troubleshooting)
       (let ((troubleshooting-buffer (get-buffer "* Car Troubleshooting Mode *")))
         (expect (buffer-live-p troubleshooting-buffer :to-be t))
         (with-current-buffer Troubleshooting-buffer
           (expect major-mode :to-be 'car-troubleshooting-mode))))
     (it "shows the appropriate first question"    
-      (car-troubleshooting-mode)
+      (car-troubleshooting)
       (expect (buffer-substring (point-min) (point-max))
               :to-equal "Is the car silent when you turn the key?"))
     (it "resets the content of the previous troubleshooting buffer"
-      (car-troubleshooting-mode)
-      (car-troubleshooting-mode)
+      (car-troubleshooting)
+      (car-troubleshooting)
       (expect (buffer-substring (point-min) (point-max))
-              :to-equal "Is the car silent when you turn the key?"))))
+              :to-equal "Is the car silent when you turn the key?"))
+    (it "displays the buffer with the troubleshooting"
+      
+      (spy-on 'display-buffer)
+      (car-troubleshooting)
+      (let ((buffer (get-argument (spy-calls-all-args 'display-buffer))))
+        (with-current-buffer buffer
+          (expect major-mode :to-be 'car-troubleshooting-mode))))))
--- car-troubleshooting-mode.el
+++ car-troubleshooting-mode.el
@@ -1,17 +1,30 @@
+(setq load-path (cons "." load-path))
+(require 'car-troubleshooting-data)
+
 (defconst car-troubleshooting-mode-buffer-name "* Car Troubleshooting Mode *")
+(defconst car-troubleshooting-mode-name "Car Troubleshooting")
 
 (defvar car-troubleshooting-mode nil
   "Mode variable for \"Car Troubleshooting Mode\"")
 
 (defun ctm--setup ()
-  )
+  (erase-buffer)
+  (insert (first ctd-list)))
 
 (defun ctm--car-troubleshooting-mode ()
   "Creates a buffer and set it to Car Troubleshooting Mode"
   (interactive)
+  (kill-all-local-variables)
+  (setq major-mode 'car-troubleshooting-mode)
+  (setq mode-name car-troubleshooting-mode-name)
   (ctm--setup))
 
 (defun car-troubleshooting ()
-  (get-buffer-create  car-troubleshooting-mode-buffer-name))
+  (let ((buffer (get-buffer-create car-troubleshooting-mode-buffer-name)))
+    (display-buffer buffer)
+    (with-current-buffer buffer
+      (ctm--car-troubleshooting-mode)))
+  
+ )
 
 (provide 'car-troubleshooting-mode)
