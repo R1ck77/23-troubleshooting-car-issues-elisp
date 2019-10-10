c8990fff49ff4f1bf67a712c5aee5789d3c22064
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-10 08:38:26 +0200

    Add a read-boolean function
    It needs refactoring

--- car-troubleshooting-mode.el
+++ car-troubleshooting-mode.el
@@ -1,3 +1,4 @@
+(require 'seq)
 (setq load-path (cons "." load-path))
 (require 'car-troubleshooting-data)
 
@@ -6,6 +7,23 @@
 
 (defvar car-troubleshooting-mode nil
   "Mode variable for \"Car Troubleshooting Mode\"")
+
+;;; TODO/FIXME belongs to another source. Also the downcase part is messy and not Open/Close
+(defconst ctm-read-boolean-positive (list "yes" "y" "t"))
+(defconst ctm-read-boolean-negative (list "no" "n" "nil"))
+(defun ctm--read-valid-input (message valid-values)
+  (let* ((valid-values (mapcar 'downcase valid-values))
+         (result (downcase (read-string message))))
+    (if (seq-contains valid-values result)
+        result
+      (ctm--read-valid-input message valid-values))))
+
+(defun ctm-read-boolean (message)
+  ; TODO/FIXME depends on the ctm--read-valid-input to deal with lowercase values… not good
+  (seq-contains ctm-read-boolean-positive
+                (ctm--read-valid-input message (append ctm-read-boolean-positive
+                                                       ctm-read-boolean-negative))))
+;;;;;
 
 (defun ctm--setup ()
   (erase-buffer)
