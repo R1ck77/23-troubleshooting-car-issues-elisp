06763906af6c1721d5c59c80c11481318106539c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-10 21:03:16 +0200

    Remove design smell

--- ctm-read.el
+++ ctm-read.el
@@ -1,19 +1,31 @@
 (require 'seq)
 
-;;; TODO/FIXME Also the downcase part is messy and not Open/Close
+
 (defconst ctm-read-boolean-positive (list "yes" "y" "t"))
 (defconst ctm-read-boolean-negative (list "no" "n" "nil"))
-(defun ctm--read-valid-input (message valid-values)
-  (let* ((valid-values (mapcar 'downcase valid-values))
-         (result (downcase (read-string message))))
-    (if (seq-contains valid-values result)
+
+(defun ctm--read-valid-input (message validator-function)
+  (let ((result (read-string message)))
+        (if (funcall validator-function result)
         result
-      (ctm--read-valid-input message valid-values))))
+        (ctm--read-valid-input message validator-function))))
+
+(defun ctm--answer-in-setp (possible-answers answer)
+  (let ((lowcase-answer (downcase answer)))
+    (seq-contains possible-answers lowcase-answer)))
+
+(defun ctm--is-positivep (answer)
+  (ctm--answer-in-setp ctm-read-boolean-positive answer))
+
+(defun ctm--is-negativep (answer)
+  (ctm--answer-in-setp ctm-read-boolean-negative answer))
+
+(defun ctm--validator (answer)
+  (or (ctm--is-positivep answer)
+      (ctm--is-negativep answer)))
 
 (defun ctm-read-boolean (message)
-  ; TODO/FIXME depends on the ctm--read-valid-input to deal with lowercase values… not good
-  (seq-contains ctm-read-boolean-positive
-                (ctm--read-valid-input message (append ctm-read-boolean-positive
-                                                       ctm-read-boolean-negative))))
+  (ctm--is-positivep
+   (ctm--read-valid-input message 'ctm--validator)))
 
 (provide 'ctm-read)
